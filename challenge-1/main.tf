9f511c1e15be232112767174bc78150659295c11
Zeal Vora <instructors@kplabs.in>
2023-07-31 15:09:22 +0530

    Create main.tf

---
+++ challenge-1/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  version = "~> 2.54"
+  region  = "us-east-1"
+  access_key = "AKIAIOSFODNN7EXAMPLE"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+}
+
+provider "digitalocean" {}
+
+terraform {
+    required_version = "0.12.31"
+}
+
+
+resource "aws_eip" "kplabs_app_ip" {
+  vpc      = true
+}
